10925de149011cbf2c69f0921f898c9599070996
widcardw <lui747353367@qq.com>
2025-03-03 11:12:44 +0800

    update icons

--- src/versions.ts
+++ src/versions.ts
@@ -1,6 +1,6 @@
 const mainVersion = 'v3.1'
 const versions = ['v2.7', 'v3.1']
-const lastUpdate = '2025.2.19'
-const faviconPath = '/curio/1168.webp'
+const lastUpdate = '2025.3.3'
+const faviconPath = '/curio/420004.webp'
 
 export { mainVersion, versions, lastUpdate, faviconPath }
