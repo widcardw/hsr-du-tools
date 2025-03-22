9ec990175088dec92acc3e9e4e81121f9694d6e5
widcardw <lui747353367@qq.com>
2025-03-22 17:41:40 +0800

    update 2.7 legacy data

--- merge_blessings/merge_desc.py
+++ merge_blessings/merge_desc.py
@@ -1,9 +1,9 @@
 import json5
 
 def merge_blessings():
-    with open("_todo_merge/blessings_new.json5", 'r') as fnew, \
-        open("_todo_merge/blessings_old.json5", 'r') as fold, \
-        open('_todo_merge/blessings_merged.json5', 'w') as fout:
+    with open("todo_merge/blessings_new.json5", 'r') as fnew, \
+        open("todo_merge/blessings_old.json5", 'r') as fold, \
+        open('todo_merge/blessings_merged.json5', 'w') as fout:
             jnew = json5.load(fnew)
             jold = json5.load(fold)
 
@@ -19,21 +19,26 @@ def merge_blessings():
             json5.dump(jold, fout, ensure_ascii=False, indent=2)
 
 def merge_equation():
-    with open('_todo_merge/equation_new.json5', 'r') as fnew, \
-        open('_todo_merge/equation_old.json5', 'r') as fold, \
-        open('_todo_merge/equation_merged.json5', 'w') as fout:
+    with open('todo_merge/equations_new.json5', 'r') as fnew, \
+        open('todo_merge/equations_old.json5', 'r') as fold, \
+        open('todo_merge/equations_merged.json5', 'w') as fout:
             jnew: list = json5.load(fnew)
             jold: list = json5.load(fold)
 
-            # jnew_1 = sorted(jnew, key=lambda x: x['_id'])
-            # jold_1 = sorted(jold, key=lambda x: x['_id'])
+            jnew = sorted(jnew, key=lambda x: x['Name'])
+            jold = sorted(jold, key=lambda x: x['Name'])
 
-            # assert(list(map(lambda x: x['_id'], jold))==list(map(lambda x: x['_id'], jold_1)))
-            # assert(list(map(lambda x: x['_id'], jnew))==list(map(lambda x: x['_id'], jnew_1)))
+            jnew_id = set(map(lambda x: x['Name'], jnew))
+            jold_id = set(map(lambda x: x['Name'], jold))
+
+            print(jold_id.difference(jnew_id))
 
             for i in range(len(jold)):
+                assert jold[i]['Name'] == jnew[i]['Name']
                 jold[i]['Desc'] = jnew[i]['Desc'][0]
+                jold[i]['_id'] = jnew[i]['_id']
 
+            jold = sorted(jold, key=lambda x: x['_id'])
             json5.dump(jold, fout, ensure_ascii=False, indent=2)
 
 if __name__ == '__main__':
--- src/versions.ts
+++ src/versions.ts
@@ -1,6 +1,6 @@
 const mainVersion = 'v3.1'
 const versions = ['v2.7', 'v3.1']
-const lastUpdate = '2025.3.13'
+const lastUpdate = '2025.3.22'
 const faviconPath = '/curio/420004.webp'
 
 export { mainVersion, versions, lastUpdate, faviconPath }
